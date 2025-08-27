ba3938e853d5b458051bbce7ced85f220d479722
Javier Anthony Jacobo <jjacobo1912@gmail.com>
2025-08-26 22:41:24 -0700

    Update Lab1.py
    Moved the docstring to the top of the assignment and provided a description to what changes were made to check_input.py

--- lab_1/Lab1.py
+++ lab_1/Lab1.py
@@ -3,14 +3,7 @@
 
     Student 1: Javier, Jacobo
     Student 2: Brian, Bayani
-
-    A brief program description
-"""
-
-import random as rand
-import check_input
-
-"""
+    
     Creates a random integer between (1-100).
     Checks input validity via check_input module.
     Keeps count of all valid guesses user makes.
@@ -20,12 +13,17 @@ import check_input
         Random_integer (int): Random generated integer within range 1-100
     Returns:
         Number of valid tries user made
+    Changes to check_input.py:
+        Added lines of code 81 & 84 to prompt the user "Guess again (1-100) when inputting invalid data.
 """
+
+import random as rand
+import check_input
 
 if __name__ == '__main__':
     random_int = rand.randint(1,100)
 
-    # To debug use this following line of code:
+    # To debug/see random integer use this following line of code:
     # print(random_int)
 
     guess = check_input.get_int_range("Im thinking of a number. Make a guess (1-100): ", 1, 100)
