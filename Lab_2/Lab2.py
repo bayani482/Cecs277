1d70524da3079a846bd4ffe4cc73893dcdc86110
Javier <jjacobo1912@gmail.com>
2025-09-04 21:35:44 -0700

    Created a list to display queen's location more cohesively

--- Lab_2/Lab2.py
+++ Lab_2/Lab2.py
@@ -7,12 +7,12 @@
 This is a gambling game where the user needs to wager money and select a one of three cards where the queen is located. 
 Each of the user input is validated. The user can decide to continue playing after each play or until they run out of money.
 
+
 """
 
 import random as rand
 
 import check_input
-
 
 def get_users_bet(money):
     """
@@ -24,8 +24,9 @@ def get_users_bet(money):
     Returns:
         returns user bet in
     """
-    print(f"You have ${money}.")
-    bet = check_input.get_int_range("How much do you wanna bet? ", 1, money)
+
+    print(f"\nYou have ${money}")
+    bet = check_input.get_int_range("How much do you wonna bet? ", 1, money)
     return bet
 
 def get_users_choice():
@@ -36,8 +37,9 @@ def get_users_choice():
     Returns:
         returns the validated user choice
     """
-    
-    print("""
+
+    print(
+"""
 +-----+ +-----+ +-----+
 |     | |     | |     |
 |  1  | |  2  | |  3  |
@@ -45,53 +47,47 @@ def get_users_choice():
 +-----+ +-----+ +-----+""")
     user_choice = check_input.get_int_range("Find the queen: ", 1, 3)
     return user_choice
-
+    
 def display_queen_loc(queen_loc):
+
     """
     Prints out to screen location of the queen
 
     Args:
-        x (int): int value between 1-3 of location of the queen
+        queen_loc (list): List with location of queen at randomly generated index (0-2) from main.
     """
-    if queen_loc ==1:
-        position_1 = "Q"
-        position_2 = "K"
-        position_3  = "K"
-    elif queen_loc == 2:
-        position_1  = "K"
-        position_2 = "Q"
-        position_3  = "K"
-        
-    elif queen_loc == 3:
-        position_1  = "K"
-        position_2  = "K"
-        position_3  = "Q"
-        
     print("+-----+ +-----+ +-----+")
     print("|     | |     | |     |")
-    print(f"|  {position_1}  | |  {position_2}  | |  {position_3}  |")
+    print(f"|  {queen_loc[0]}  | |  {queen_loc[1]}  | |  {queen_loc[2]}  |")    # Print out all elements of the "queen_list" list.
     print("|     | |     | |     |")
     print("+-----+ +-----+ +-----+")
-    
 
 def main():
     print("-- Three Card Monte --")
     print("Find the queen to double your bet!")
+    print("-----------------------------------")
     play = True
     user_money = 100
 
     while play:
-        queenLoc = rand.randint(1,3)
-        print(queenLoc)
+
+        queen_list = ["K","K","K"]  # Create/Reset a list named "queen_list" of 3 elements, all "K".
+        queen_location = rand.randint(1,3)
+        queen_list[queen_location - 1] = "Q"    #Change index at "queen_location" (random number from 1-3)
+
+        # To debug and find the queen's location, uncomment the line of code below
+        print(f"Queen's Location: {queen_location}")
+
         bet = get_users_bet(user_money)
         users_Choice = get_users_choice()
-        display_queen_loc(queenLoc)
-        if queenLoc == users_Choice:
+        display_queen_loc(queen_list)
+
+        if queen_location == users_Choice:
             user_money += bet
-            print(f"you got lucky this time ...\n you have ${user_money}.")
+            print(f"You got lucky this time ...\n")
         else:
             user_money -= bet
-            print("Sorry... you lose")
+            print("Sorry... you lose\n")
         
         if user_money == 0:
             print("You're out of money. Beat it loser!")
@@ -101,4 +97,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
