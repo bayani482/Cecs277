ae72a692c973c74d56fb6ab1817dc2531598018c
Brian B <103539309+bayani482@users.noreply.github.com>
2025-09-15 14:26:27 -0700

    modified:   Lab4.py 	modified:   __pycache__/check_input.cpython-312.pyc

--- Lab_4/Lab4.py
+++ Lab_4/Lab4.py
@@ -8,32 +8,34 @@
 
 import check_input
 
+
 def read_maze():
     maze_2d = []
-
     with open("maze.txt", "r") as maze:
         for row in maze:
-            items = row.strip().split(" ")
+            items = row.strip()
             maze_2d.append(items)
-
-    for row in maze_2d:
-        print (" ".join(map(str,row)))
-
     return maze_2d
 
 def find_start(maze):
+    for row_index, row in enumerate(maze):
+        for col_index, value in enumerate(row):
+            if value == "s":
+                startRowIndex = row_index
+                startColIndex = col_index
+    return startRowIndex, startColIndex
+
+
+def display_maze(maze, loc):
+    print(loc)
     for row in maze:
-        for column in row:
-            if 's' == column:
-                index_of_s = maze.index(column)
-                print(index_of_s)
-
-#def display_maze(maze, loc):
-
+        for element in row:
+            print(element, end=' ')
+        print()
+    
 def main():
-
     maze = read_maze()
-    find_start(maze)
+    display_maze(maze,find_start(maze))
     
 if __name__ == '__main__':
     main()
