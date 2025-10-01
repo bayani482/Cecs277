61d585695671e2d4e16c58464ec1d72f58f0538c
Brian B <103539309+bayani482@users.noreply.github.com>
2025-09-30 19:41:50 -0700

    new file:   __pycache__/check_input.cpython-312.pyc 	new file:   __pycache__/die.cpython-312.pyc 	new file:   __pycache__/player.cpython-312.pyc 	new file:   check_input.py 	new file:   die.py 	new file:   main.py 	new file:   player.py

---
+++ Lab_6/main.py
@@ -0,0 +1,41 @@
+"""
+LAB 5
+
+Student 1: Javier Jacobo
+Student 2: Bryan Bayani
+
+This program is a game that rolls three dice and checks for either a pair, series, or three of a kind. You will earn points based on
+what combination you roll. The user can play as many times as they want  awnser Y to continue or N to stop.
+
+"""
+import check_input
+from player import Player
+
+
+def take_turn(player):
+    print("-Yahtzee-")
+    player.roll_dice()
+    print(player)
+    if player.has_three_of_a_kind():
+        print(f"Three of a Kind of {player._dice[0]._value}!")
+    elif player.has_series():
+        print(f"You got a Series of {player._dice[0]._value}!")
+    elif player.has_pair():
+        print("You got a Pair!")
+    else:
+        print("Aww. Too Bad.")
+        
+    print(f"Score: {player._points}\n")
+    
+def main():
+    player = Player()
+    play = True
+    while play == True:
+        take_turn(player)
+        play = check_input.get_yes_no("Play again? (Y/N): ")
+
+
+    print(f"Game Over\nFinal Score = {player._points}")
+
+if __name__ == "__main__":
+    main()
---
+++ Lab_6/player.py
@@ -0,0 +1,85 @@
+"""
+LAB 6
+player class
+
+Student 1: Javier Jacobo
+Student 2: Bryan Bayani
+
+This class creates a player object that has a list of 3 dices rolls and initials points to keep track of the score
+"""
+from die import Die
+
+
+class Player:
+    
+    def __init__(self):
+        """
+        Initializes a Player object with three dice and zero points.
+        
+        """
+        self._dice = [Die(), Die(),Die()]
+        self._points = 0
+    @property
+    def points(self):
+        """
+        Returns the player's current points.
+        Returns:
+            int: The player's current points.
+        """
+        return self
+    
+    def roll_dice(self):
+        """
+        rolls three dice and sorts them in ascending order.
+        
+        Returns:
+            list: list of 3 dice rolls
+        """
+        for d in self._dice:
+            d.roll()
+        self._dice.sort()
+        return self._dice
+    def has_pair(self):
+        """checks dice list to see if there is a pair if so adds 1 point to the players score
+
+        Returns:
+            bool: True if there is a pair, False otherwise.
+        """
+        for i in range(len(self._dice)):
+            for j in range(i + 1, len(self._dice)):
+                if self._dice[i] == self._dice[j]:
+                    self._points += 1
+                    return True
+        return False
+    def has_three_of_a_kind(self):
+        """checks dice list to see if there is a three of a kind if so adds 3 point to the players score
+
+        Returns:
+            bool: True if there is a pair, False otherwise.
+        """
+        if self._dice[0] == self._dice[1] and self._dice[1] == self._dice[2]:
+            self._points += 3
+            return True
+        else:
+            return False
+    def has_series(self):
+        """checks dice list to see if there is a series if so adds 2 point to the players score
+
+        Returns:
+            bool: True if there is a pair, False otherwise.
+        """
+        values = [d._value for d in self._dice]
+        values.sort()
+        if values[1] == values[0] + 1 and values[2] == values[1] + 1:
+            self._points += 2
+            return True
+        else:
+            return False
+    def __str__(self):
+        """changes the player object to a string representation of the dice rolls
+
+        Returns:
+            str: formatted string of the dice rolls
+        """
+        return f"D1={self._dice[0]._value}, D2={self._dice[1]._value}, D3={self._dice[2]._value}"
+
