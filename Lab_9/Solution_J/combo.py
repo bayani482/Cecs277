061397b8a4db68ea6f7e86590d9daa919cec3ff2
Javier <jjacobo1912@gmail.com>
2025-10-21 15:18:24 -0700

    10/21 Javier

--- Lab_9/Solution_J/combo.py
+++ Lab_9/Solution_J/combo.py
@@ -1,7 +1,34 @@
 from door import Door
-import random
+import random as rand   
 
 class ComboDoor(Door):
     def __init__(self):
-        super().__init__()
+        self._combo = rand.randint(1,10)
+        self._input = 0
     
+    def examine_door(self):
+        return "A door with a combination lock. You can spin the dial to a number 1-10."
+    
+    def menu_options(self):
+        return "Enter # 1-10:"
+    
+    def get_menu_max(self):
+        return 10
+    
+    def attempt(self,option):
+        self._input = option
+        return f"You spin the dial to {self._input}."
+    
+    def is_unlocked(self):
+        if self._input == self._combo:
+            return True
+        else:
+            return False
+        
+    def clue(self):
+        if self._input < self._combo:
+            return "Too low."
+        else:
+            return "Too high."
+    def success(self):
+        return "Congratulations! You opened the combo lock door."
