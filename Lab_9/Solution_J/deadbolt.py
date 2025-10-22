7f93ad937b79ef7c2af5ec72798cf46eb2d9e76a
Javier <jjacobo1912@gmail.com>
2025-10-22 11:38:45 -0700

    10/22

---
+++ Lab_9/Solution_J/deadbolt.py
@@ -0,0 +1,44 @@
+from door import Door
+import random as rand
+
+class DeadboltDoor(Door):
+    def __init__(self):
+        self._state = rand.randint(0,1), rand.randint(0,1)  # 0 = locked, 1 = unlocked
+        self._input = 0
+    def examine_door(self):
+        return """
+A door with two deadbolts.
+Both need to be unlocked to open the door,
+but you can't tell if each one is locked or unlocked."""
+
+    def menu_options(self):
+        return "1. Toggle bolt 1\n2. Toggle bolt 2"
+    
+    def get_menu_max(self):
+        return 2
+    
+    def attempt(self,option):
+        self._input = option
+        match self._input:
+            case 1:
+                self._state = (1 - self._state[0], self._state[1])
+                return "You toggle the first bolt."
+            case 2:
+                self._state = (self._state[0], 1 - self._state[1])
+                return "You toggle the second bolt."
+
+    def is_unlocked(self):
+        if self._state == (1,1):
+            return True
+        else:
+            return False
+    
+    def clue(self):
+        if self._state == (1,0) or self._state == (0,1):
+            return "You jiggle the door... it seems like one of the bolts is unlocked."
+        else:
+            return "...it seems like it's completely locked"
+    
+    def success(self):
+        return "Congratulations! You unlocked both deadbolts and opened the door."
+    
--- Lab_9/Solution_J/main.py
+++ Lab_9/Solution_J/main.py
@@ -1,5 +1,6 @@
 from basic_door import BasicDoor
 from combo import ComboDoor
+from deadbolt import DeadboltDoor
 from door import Door
 import check_input
 import random as rand   
@@ -14,16 +15,18 @@ def open_door(door):
         
         if not door.is_unlocked():
             print(door.clue())
-    
-    print(door.success())
+
+    print(f"{door.success()}\n")
 
 def main():
     doors_unlocked = 0
 
+    print("Welcome to the Escape Room.\nYou must unlock 3 doors to escape...")
     while doors_unlocked < 3:
-        doors = [BasicDoor(), ComboDoor()]
+        doors = [BasicDoor(), ComboDoor(), DeadboltDoor()]
         open_door(rand.choice(doors))
         doors_unlocked += 1
 
+    print("Congratulations! You have escaped... this time.")
 if __name__ == "__main__":
     main()
