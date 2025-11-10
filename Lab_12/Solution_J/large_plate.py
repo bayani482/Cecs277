b9a622cfc3833ee394052d91b1cb949f622e139e
Javier <jjacobo1912@gmail.com>
2025-11-10 09:34:09 -0800

    11/10

---
+++ Lab_12/Solution_J/large_plate.py
@@ -0,0 +1,15 @@
+from plate import Plate
+
+class LargePlate(Plate):
+
+    def decscription(self):
+        return "This is a large plate"
+    
+    def area(self):
+        return 113
+    
+    def weight(self):
+        return 24
+    
+    def count(self):
+        return 0
---
+++ Lab_12/Solution_J/potatoes.py
@@ -0,0 +1,16 @@
+from plate_decorator import PlateDecorator
+
+class Potatoes(PlateDecorator):
+
+    def description(self):
+        return super().description() + ", with potatoes"
+    
+    def area(self):
+        return super().area() - 18
+    
+    def weight(self):
+        return super().weight() - 6
+        
+    def count(self):
+        return super().count() + 1
+    
